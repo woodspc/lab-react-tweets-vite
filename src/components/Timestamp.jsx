6e2ff6e42d0c873b1a75a7276b341c3f4b93b528
Peter Woods <woodspc@gmail.com>
2024-08-04 12:50:31 +0100

    done

---
+++ src/components/Timestamp.jsx
@@ -0,0 +1,3 @@
+export default function ({ time }) {
+  return <span className="timestamp">{time}</span>;
+}
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,34 +1,29 @@
-function Tweet() {
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
+
+function Tweet({ tweet }) {
+  const IMAGE_URL = tweet.user.image;
+  const USER_NAME = tweet.user.name;
+  const USER_HANDLE = tweet.user.handle;
+  const TWEET_TIMESTAMP = tweet.timestamp;
+  const TWEET_MESSAGE = tweet.message;
+
   return (
     <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={IMAGE_URL} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+          <User name={USER_NAME} handle={USER_HANDLE} />
+          <Timestamp time={TWEET_TIMESTAMP} />
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        <Message message={TWEET_MESSAGE} />
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
+        <Actions />
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
